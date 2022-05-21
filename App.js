0b4837c194b466fdc7b045c44f6c7f658581144e
Federico <fedeaacevedo@gmail.com>
2022-05-21 01:25:47 -0300

    modal

--- App.js
+++ App.js
@@ -1,112 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Text, SafeAreaView, StyleSheet, Pressable, Modal } from 'react-native';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [modalVisible, setModalVisible] = useState(false)
+
+  console.log(modalVisible)
+
+  const nuevaCitaHandle = () => {
+    console.log('Click..')
+  }
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+
+
+    <SafeAreaView style={styles.container}>
+
+      <Text style={styles.titulo}> Administrador de
+        Citas {''}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
+
+      <Pressable
+        onPress={nuevaCitaHandle}
+        style={styles.btnNuevaCita}
+      >
+
+        <Text style={styles.btnTextoNuevaCita}>Nueva cita</Text>
+
+      </Pressable>
+
+      <Modal
+        animationType='slide'
+        visible={false}
+      >
+        <Text>Desde Modal</Text>
+      </Modal>
+
     </SafeAreaView>
-  );
+
+
+  )
 };
+
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    backgroundColor: '#f3f4f6',
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6d28d9'
   },
-  highlight: {
-    fontWeight: '700',
+  btnNuevaCita:{
+    backgroundColor:'#6D28D9',
+    padding: 15,
+    marginTop:20,
+    marginHorizontal: 20,
+    borderRadius: 30
   },
-});
+  btnTextoNuevaCita:{
+    textAlign:'center',
+    color:'#fff',
+    fontSize: 20,
+    textTransform:'uppercase',
+    fontWeight:'bold'
+  }
+})
+
 
 export default App;
